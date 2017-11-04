d4e59074f84232a66e066220d6dcd33a40e0c81a
Miguel Rust <hava12soft@gmail.com>
2017-11-03 20:43:19 -0600

    Removed migrations that created a conflict>

--- db/migrate/20170927021225_remove_pending_items_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemovePendingItemsFromUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :pendingItems, :array
-  end
-end
--- db/migrate/20170927021255_remove_pending_item_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemovePendingItemFromUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :pendingItem, :collection
-  end
-end
